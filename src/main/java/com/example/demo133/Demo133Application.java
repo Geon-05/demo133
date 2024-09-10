3e302d24cbbc771a207f2f1422646893de65eee6
geon <zqrc05@gmail.com>
2024-09-10 18:46:04 +0900

    first commit

---
+++ src/main/java/com/example/demo133/Demo133Application.java
@@ -0,0 +1,13 @@
+package com.example.demo133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo133Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo133Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo133/Demo133ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo133;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo133ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
